a531ad0848bb063074479c56ecfd89b18fe1ac45
JAGAT8077 <jagatprakash382@gmail.com>
2026-04-08 11:50:02 +0530

    Update payment.js

--- payment.js
+++ payment.js
@@ -2,14 +2,20 @@ exports.handler = async function(event, context) {
   try {
     const data = JSON.parse(event.body);
 
-    const response = await fetch("https://api.upigateway.com/create-order", {
-      method: "POST",
+    const response = await fetch('https://merchant.upigateway.com/api/create_order', {
+      method: 'POST',
       headers: {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
+        key: "15ff4049-df56-40ce-b9c0-4f7b1d179f7d",
+        client_txn_id: data.orderId,
         amount: data.amount,
-        name: data.name
+        p_info: data.info,
+        customer_name: data.name,
+        customer_email: "customer@smartprint.com",
+        customer_mobile: "9999999999",
+        redirect_url: data.redirectUrl
       })
     });
 
